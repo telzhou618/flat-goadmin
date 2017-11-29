7c2033519472dde868febb74e8c0cfd939878341
zhougaojun618 <274410087@qq.com>
2017-11-30 00:33:17 +0800

    menu

--- controllers/base.go
+++ controllers/base.go
@@ -2,26 +2,35 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/beego/wetalk/modules/utils"
 )
+
+var size int = 15 //默认页大小，取app.conf文件中的page_size
 
 const (
 	layout string = "common/layout.html"
-	size int = 10
 )
 
 type BaseController struct {
 	beego.Controller
 }
 
-func (c *UserController) Prepare()  {
+//初始化基础数据，启动执行
+func init() {
+	 size,_= beego.AppConfig.Int("page_size")
+}
+
+//设置默认模板，每次请求执行
+func (c *BaseController) Prepare()  {
+	//全局验证处理
 
 }
 
-
 //转JSON
-func (c *BaseController)Rsp(success bool,str string)  {
+func (c *BaseController)Rsp(success bool,str interface{})  {
 	c.Data["json"] = &map[string]interface{}{"success":success,"data" : str}
 	c.ServeJSON()
+	c.StopRun()
 }
 
 //是否为GET请求
@@ -44,6 +53,35 @@ func (c *BaseController) IsPOST() bool {
 	}
 }
 
+//重定向302
 func (c *BaseController) Redirect302(url string) {
 	c.Redirect(url,302)
+	return
+}
+
+//渲染到布局+模板
+func (c *BaseController) renderLayTpl(layout,url string) {
+	c.Layout = layout
+	c.TplName = url
+	return
+}
+//渲染到模板
+func (c *BaseController) renderTpl(url string) {
+	c.Layout = layout
+	c.TplName = url
+	return
+}
+
+//渲染分页列表
+/**
+ size 页大小
+  total 总记录数
+ */
+func (c *BaseController) renderPageTpl(data interface{},total int64,url string) {
+	paginator := utils.NewPaginator(c.Ctx.Request, size, total)
+	c.Data["data"] = data
+	c.Data["paginator"] = paginator
+	c.Layout = layout
+	c.TplName = url
+	return
 }
--- controllers/menu.go
+++ controllers/menu.go
@@ -2,33 +2,40 @@ package controllers
 
 import (
 	m "flat-goadmin/models"
-	"github.com/beego/wetalk/modules/utils"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 )
 
 type MenuController struct {
 	BaseController
 }
 
+
 func (this *MenuController) Index()  {
+	beego.ReadFromRequest(&this.Controller)
 	page,_ := this.GetInt("p",1)
 	search := this.GetString("s","")
+
 	this.Data["s"] = search
 	filters := make(map[string] interface{})
 	if !(search == ""){
 		filters["text__icontains"]=search
 	}
-	roles,total := m.MenuPageList(page,size,filters)
-
-	paginator := utils.NewPaginator(this.Ctx.Request, size, total)
-	this.Data["data"] = roles
-	this.Data["paginator"] = paginator
-	this.Layout = layout
-	this.TplName = "menu/list.html"
+	data,total := m.MenuPageList(page,size,filters)
+	this.renderPageTpl(data,total,"menu/list.html")
 }
 
 func (this *MenuController) Add()  {
 	if this.IsGet(){
 		this.Layout = layout
 		this.TplName = "menu/add.html"
+	}else {
+		menu := m.Menu{}
+		this.ParseForm(&menu)
+		orm.NewOrm().Insert(&menu)
+		flash := beego.NewFlash()
+		flash.Success("保存成功！")
+		flash.Store(&this.Controller)
+		this.Redirect302("/menu")
 	}
 }
